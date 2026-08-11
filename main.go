ff8bff2a44968ff01b2804f8b2d9299939589776
Jan Kejř <jan.kejr@centrum.cz>
2026-08-11 10:31:27 +0200

    refactor: rewire main onto internal packages, delegate to plain ssh

--- main.go
+++ main.go
@@ -1,36 +1,50 @@
+// Command hopper is a TUI for picking an SSH host from ~/.ssh/config
+// (including Include files) and connecting to it.
 package main
 
 import (
 	"fmt"
 	"os"
-
-	"github.com/kejrak/hopper/config"
-	"github.com/kejrak/hopper/hosts"
+	"path/filepath"
 
 	"github.com/ktr0731/go-fuzzyfinder"
+
+	"github.com/kejrak/hopper/internal/host"
+	"github.com/kejrak/hopper/internal/sshcfg"
 )
 
 func main() {
-	cfg, err := config.ParseAllConfigs()
-	if err != nil {
-		fmt.Println("Could not parse SSH config:", err)
-		os.Exit(1)
-	}
+	os.Exit(run())
+}
 
-	hosts := hosts.GetSSHHosts(cfg)
+func run() int {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "hopper:", err)
+		return 1
+	}
+	sshDir := filepath.Join(home, ".ssh")
+	root := filepath.Join(sshDir, "config")
+
+	hosts, warnings, err := sshcfg.Hosts(root, sshDir)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "hopper:", err)
+		return 1
+	}
+	for _, w := range warnings {
+		fmt.Fprintln(os.Stderr, "hopper: warning:", w)
+	}
 	if len(hosts) == 0 {
-		fmt.Println("No valid hosts found.")
-		return
+		fmt.Fprintln(os.Stderr, "hopper: no hosts found in", root)
+		return 1
 	}
 
 	idx, err := fuzzyfinder.Find(hosts, func(i int) string {
-		h := hosts[i]
-		return fmt.Sprintf("%s (%s@%s:%s)", h.Name, h.User, h.Hostname, h.Port)
+		return hosts[i].Display()
 	})
 	if err != nil {
-		fmt.Println("Host selection canceled.")
-		return
+		// User cancelled the picker (esc / ctrl+c).
+		return 0
 	}
-
-	hosts[idx].Connect()
+	return host.ExitCode(host.Command(hosts[idx].Name).Run())
 }
--- makefile
+++ makefile
@@ -1,5 +1,18 @@
-build: 
-	go build -o bin/hopper main.go
+BIN := bin/hopper
+
+build:
+	go build -o $(BIN) .
 
 run: build
-	./bin/hopper
+	./$(BIN)
+
+fmt:
+	gofmt -w .
+
+vet:
+	go vet ./...
+
+test:
+	go test ./...
+
+.PHONY: build run fmt vet test
